8b6f4b853324430c162bd67a3c98166321507b23
Vasyl Kobyletskiy <v.kobyletskiy@viseven.com>
2023-11-08 08:19:38 +0200

    add lesson 1

---
+++ lesson1/main.py
@@ -0,0 +1,15 @@
+# First task
+#name = input('Please enter your name:')
+#print(f'Hello {name}!')
+
+#Second task
+firstDigit = int(input('Enter first digit'))
+secondDigit = int(input('Enter second digit'))
+thirdDigit = int(input('Enter third digit'))
+fourthDigit = int(input('Enter fourth digit'))
+fifthDigit = int(input('Enter fifth digit'))
+print(f'min value: {min(firstDigit, secondDigit, thirdDigit, fourthDigit, fifthDigit)}')
+print(f'max value: {max(firstDigit, secondDigit, thirdDigit, fourthDigit, fifthDigit)}')
+print(f'average: {(firstDigit + secondDigit + thirdDigit + fourthDigit + fifthDigit)/5}')
+
+#Third task
