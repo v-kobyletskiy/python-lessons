401aa2e3df41d471b83bec0c2f1745630c86f057
Vasyl Kobyletskiy <v.kobyletskiy@viseven.com>
2023-11-09 03:43:27 +0200

    lesson 1

--- lesson1/main.py
+++ lesson1/main.py
@@ -1,6 +1,6 @@
 # First task
-#name = input('Please enter your name:')
-#print(f'Hello {name}!')
+name = input('Please enter your name:')
+print(f'Hello {name}!')
 
 #Second task
 firstDigit = int(input('Enter first digit'))
@@ -13,3 +13,35 @@ print(f'max value: {max(firstDigit, secondDigit, thirdDigit, fourthDigit, fifthD
 print(f'average: {(firstDigit + secondDigit + thirdDigit + fourthDigit + fifthDigit)/5}')
 
 #Third task
+x = int(input('Enter first operand'))
+y = int(input('Enter second operand'))
+print(f'x + y: {x + y}')
+print(f'x - y: {x - y}')
+print(f'x * y: {x * y}')
+print(f'x / y: {x / y}')
+print(f'x // y: {x // y}')
+
+#Fourth task
+PI = 3.1415
+circleRadius = int(input('Enter circle radius'))
+print(f'Diametr: {circleRadius * 2}')
+print(f'Circle length: {2 * PI * circleRadius}')
+print('Circle square', PI * (circleRadius ** 2))
+
+#Fifth task
+startAmount = 1000
+percentIncome = 0.1
+print(f'Amount of money in 10 years: {startAmount * (1 + percentIncome) ** 10}')
+print(f'Amount of money in 20 years: {startAmount * (1 + percentIncome) ** 20}')
+print(f'Amount of money in 30 years: {startAmount * (1 + percentIncome) ** 30}')
+
+#Sixth task
+dollarExchangeRate = 37.7
+dollarsAmount = int(input('Please enter dollars amount: '))
+print(f'Amount dollars in hrivnas: {dollarsAmount * dollarExchangeRate}')
+
+#Seventh task
+threeDigitNumber = int(input('Please enter three digit number'))
+print(threeDigitNumber // 100)
+print(threeDigitNumber // 10 % 10)
+print(threeDigitNumber % 10)
