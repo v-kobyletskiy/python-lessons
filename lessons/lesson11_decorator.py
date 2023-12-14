e59212d69bb647b36fd14672ee8b702ec3d1925a
Vasyl Kobyletskiy <vasyl.kobyletskiy@gmail.com>
2023-12-14 13:53:41 +0200

    lesson 11 decorators

--- lessons/lesson11_decorator.py
+++ lessons/lesson11_decorator.py
@@ -1,11 +1,11 @@
 # Task 1
-# def transform_digits(digits, transformer):
-#     transformed_digits = map(transformer, digits)
-#     return sum(transformed_digits)
-#
-#
-# digits_to_transform = [1, 2, 3]
-# print(f'Sum of {[1, 2, 3]} increased by \'1\': {transform_digits(digits_to_transform, lambda item: item + 1)}')
+def transform_digits(digits, transformer):
+    transformed_digits = map(transformer, digits)
+    return sum(transformed_digits)
+
+
+digits_to_transform = [1, 2, 3]
+print(f'Sum of {[1, 2, 3]} increased by \'1\': {transform_digits(digits_to_transform, lambda item: item + 1)}')
 
 # Task 2
 def read_cache(file_path):
@@ -35,3 +35,63 @@ def count_fibonacci(number):
 
 print(count_fibonacci(5))
 print(count_fibonacci(4))
+
+# Task 3
+import time
+def measure_time(func):
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        res = func(*args, **kwargs)
+        end = time.perf_counter()
+        print(f'Time function\'{func.__name__}\' evaluation: {end - start:.2f} seconds')
+        return res
+
+    return wrapper
+
+@measure_time
+def test_function():
+    time.sleep(2)
+
+test_function()
+
+# Task 4
+def limit_calls(max_calls):
+    def decorator(func):
+        calls_counter = 0
+        def wrapper(*args, **kwargs):
+            nonlocal calls_counter
+            if calls_counter >= max_calls:
+                print(f'Exceeds call function \'{func.__name__}\' limit')
+            else:
+                calls_counter += 1
+                return func(*args, **kwargs)
+
+        return wrapper
+    return decorator
+@limit_calls(2)
+def limit_calls_function():
+    print('Call limit_calls_function')
+
+limit_calls_function()
+limit_calls_function()
+limit_calls_function()
+
+# Task 5
+def cache_result(func):
+    def wrapper(*args, **kwargs):
+        cache_key = args
+        if cache_key not in wrapper.cache:
+            wrapper.cache[cache_key] = func(*args, **kwargs)
+        return wrapper.cache[cache_key]
+
+    wrapper.cache = dict()
+    return wrapper
+
+@cache_result
+def fibonacci(number):
+    if number <= 1:
+        return 1
+    return fibonacci(number - 1) + fibonacci(number - 2)
+
+print(fibonacci(5))
+print(fibonacci(5))
